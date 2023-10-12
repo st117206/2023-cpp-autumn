c2f9c7af0378e5c83934f53687653ae0f2b82adf
master <st117206@student.spbu.ru>
2023-10-12 22:25:04 +0300

    Test 1

---
+++ Test1/Task2/Task2.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+int main()
+{
+    int n = 0;
+
+    std::cin >> n;
+    for (int i = 0; i <= n; i++)
+    {
+        std::cout << i << " ";
+    }
+    for (int i = n - 1; i >= 0; i--)
+    {
+        std::cout << i << " ";
+    }
+}
---
+++ Test1/Task3/Task3.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int main()
+{
+    int n = 0;
+
+    std::cin >> n;
+    while (n / 10 > 0)
+    {
+        std::cout << n % 10;
+        n = n / 10;
+    }
+    std::cout << n;
+}
